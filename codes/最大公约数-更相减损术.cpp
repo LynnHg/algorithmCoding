908b145f35163703f6b591cc091dc88bb06caf99
wang <lynnhung361@163.com>
2019-03-22 16:30:39 +0800

    Create 最大公约数-更相减损术.cpp

---
+++ codes/最大公约数-更相减损术.cpp
@@ -0,0 +1,27 @@
+```C
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<math.h>
+
+int func(int a, int b) {
+  while(a != b){
+    if(a > b)
+      a -= b;
+    else 
+      b -= a;
+  }
+  return a;
+}
+
+void main(){
+	int a,b;
+
+	printf("please input a b:\n");
+	scanf("%d%d", &a, &b);
+	printf("%d和%d的最大公约数为:%d\n", a, b, func(a,b));
+
+	//防止黑框闪退
+	system("pause");
+}
+```
